1b8b1be5e2e93087db59041ef3a5a2e26b32b49a
Tej Pratap <kumarpratap.1447@gmail.com>
2021-12-26 01:02:24 +0530

    binary tree updat

--- binaryTree_4.cpp
+++ binaryTree_4.cpp
@@ -24,7 +24,49 @@ public:
     }
 };
 
+int height(BinaryTreeNode<int>* root){
+    if(root==NULL){
+        return 0;
+    }
+    return 1+max(height(root->left),height(root->right));
+}
 
+//calculating diameter of treenode
+int diameter(BinaryTreeNode<int>* root)
+{
+    if(root==NULL){
+        return 0;
+    }
+
+    int option1=height(root->left)+height(root->right);
+    int option2=diameter(root->left);
+    int option3=diameter(root->right);
+
+    return max(option1,max(option2,option3));
+}
+
+//heightDiameter- this function is used for better approach in diameter function
+pair<int,int> heightDiameter(BinaryTreeNode<int>* root){
+    if(root==NULL){
+        pair<int,int> p;
+        p.first=0;
+        p.second=0;
+        return p;
+    }
+    pair<int,int> leftAns=heightDiameter(root->left);
+    pair<int,int> rightAns=heightDiameter(root->right);
+    int ld=leftAns.second;
+    int lh=leftAns.first;
+    int rd=rightAns.second;
+    int rh=rightAns.first;
+
+    int height=1+max(lh,rh);
+    int diameter = max(lh+rh,max(ld,rd));
+    pair<int,int> p;
+    p.first=height;
+    p.second=diameter;
+    return p;
+}
 
 //printing binary tree
 void printTree(BinaryTreeNode<int>* root)
@@ -94,9 +136,14 @@ int main()
 {
     //levelwise input
     BinaryTreeNode<int>* root=takeInputLevelwise();
-    cout<<"Total number of nodes: "<<numNodes(root)<<endl;
+
     printTree(root);
-    //inorder(root);
+    cout<<"Total number of nodes: "<<numNodes(root)<<endl;
+
+
+    pair<int,int> p = heightDiameter(root);
+    cout<<"Height: "<<p.first<<endl;
+    cout<<"Diameter: "<<p.second<<endl;
     delete root;
     return 0;
 }
