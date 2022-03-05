5d4aa887818ea3be5cb1c02cacfff6473a189a6d
Tej Pratap <bankebihari5463@gmail.com>
2022-03-05 19:36:16 +0530

    header files addes

--- demo.cpp
+++ demo.cpp
@@ -1,20 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve()
+int gcd1(int A, int B)
 {
-    cout<<"test case running"<<endl;
+    if(B==0)
+        return A;
+    else
+        return gcd1(B, A % B);
 }
 
-int main()
-{
-    cout<<"---------program started---------"<<endl;
-    int testcase;
-    cin>>testcase;
-    while(testcase){
-        testcase--;
-        solve();
+int longestLength(vector<int>& arr,int n){
+    int maxLength = 0;
+    for (int i = 0; i < n; i++) {
+        int gcd = 0;
+        for (int j = i; j < n; j++) {
+            gcd = gcd1(gcd, arr[j]);
+            if (gcd > 1)
+                maxLength = max(maxLength, j - i + 1);
+            else
+                break;
+        }
     }
+    return maxLength-1;
+}
+
+
+int main() {
+    // Write C++ code here
+    cout<<"---------program started---------"<<endl;
+    vector<int> arr;
+    arr.push_back(4);
+    arr.push_back(1);
+    arr.push_back(2);
+    arr.push_back(4);
+    arr.push_back(8);
+
+
+    cout<<longestLength(arr,arr.size())<<endl;
+
 
 
     return 0;
