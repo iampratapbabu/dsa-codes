d44a3c82d58543c839eca382b2bf7e5e9672a077
Tej Pratap <bankebihari5463@gmail.com>
2022-10-13 14:12:32 +0530

    recursion part-1

--- recursion1.cpp
+++ recursion1.cpp
@@ -52,18 +52,70 @@ bool is_sorted(int a[],int size){
     }
 }
 
+int firstIndex(int a[],int size,int x){
+    if(size==0){
+        return -1;
+    }
+    if(a[0]==x){
+        return 0;
+    }
+    int ans = firstIndex(a+1,size-1,x);
+    if(ans==-1){
+        return -1;
+    }
+    else{
+        return ans+1;
+    }
+
+}
+
+int lastIndex(int a[],int size,int x){
+    if(size==0){
+        return -1;
+    }
+
+    int ans = lastIndex(a+1,size-1,x);
+    if(ans==-1){
+            if(a[0]==x)
+                return 0;
+        return -1;
+    }
+    else{
+        return ans+1;
+    }
+}
+
+int allIndices(){
+
+}
+
 int main() {
     // Write C++ code here
     cout<<"---------program started---------"<<endl;
     int n;
     cin>>n;
     int output = factorial(n);
-    cout<<output<<endl;
-    //cout<<fibonacci(n);
+    cout<<"Factorial of "<<n<<output<<endl;
+    cout<<fibonacci(n);
     cout<<fibonacciDiff(n)<<endl;
-    int a[]={1,2,3,4,5};
+    int a[]={1,2,3,2,4,5};
+    int b[]={5,6,6,5,4,5,6};
     int sizeofarray = sizeof(a)/sizeof(a[0]);
     cout<<is_sorted(a,sizeofarray)<<endl;
+    int x;
+
+    //first index
+    cout<<"enter number to find index: ";
+    cin>>x;
+    int res=firstIndex(a,sizeofarray,x);
+    cout<<"First Index of "<<x<<" is "<<res<<endl;
+
+    //Last index
+    cout<<"enter number to find index: ";
+    cin>>x;
+    res=lastIndex(b,sizeofarray,x);
+    cout<<"Last Index of "<<x<<" is "<<res<<endl;
+
     return 0;
 }
 
